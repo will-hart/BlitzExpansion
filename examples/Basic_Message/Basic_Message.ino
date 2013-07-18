247c6a61ab7b97d6ccf3c94f8a7e4f14d259130d
will-hart <hart.wl@gmail.com>
2013-07-18 19:37:05 +0100

    Move and update first example

--- examples/Basic_Message/Basic_Message.ino
+++ examples/Basic_Message/Basic_Message.ino
@@ -5,13 +5,16 @@ void setup() {
 }
 
 void loop() {
+  // set up a blitz expansion board message with board ID 2
   BlitzMessage msg(2);
-  msg.pack(729, 16);
   
-  // get the message
+  // format the message (currently no variables set)
   char formatted_message[29];
   msg.render(formatted_message);
+  
+  // write to serial
   Serial.println(formatted_message);
   
+  // rinse for 1 second, repeat
   delay(1000);
 }
